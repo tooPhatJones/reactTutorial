2b93db57e3a409a4c3722202717989887c5be204
canjalal <vincent.shuali@gmail.com>
2022-12-07 20:13:06 -0800

    Make NavLink pages more distinct

---
+++ src/NavLinks/NavLink2.js
@@ -0,0 +1,10 @@
+import React from "react";
+
+export function NavLink2() {
+    return (
+        <div>
+            <h2>Two</h2>
+            We are on NavLink 2's page. Check out the link bar above!
+        </div>
+    );
+}
---
+++ src/NavLinks/NavLink3.js
@@ -0,0 +1,10 @@
+import React from "react";
+
+export function NavLink3() {
+    return (
+        <div>
+            <h2>Three</h2>
+            We are on NavLink 3's page. Check out the link bar above!
+        </div>
+    );
+}
